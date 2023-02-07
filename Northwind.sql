71f6e7b2728f226430192c704f8b058d65e28d9b
Earle Fox <earle.fox312@gmail.com>
2023-02-06 21:11:00 -0500

    first commit

---
+++ Northwind.sql
@@ -0,0 +1,50 @@
+USE Northwind
+--1
+--SELECT * FROM Customers;
+
+--2
+--SELECT DISTINCT Country
+--FROM Customers
+
+--3
+--SELECT * FROM [Customers]--WHERE CHARINDEX('BL', CustomerID)>0 -- OR --SELECT * FROM Customers--WHERE CustomerID LIKE 'BL%';--4--SELECT TOP 100 * FROM Orders;----5--SELECT * FROM Customers--WHERE PostalCode = '1010' OR PostalCode = '3012' OR PostalCode = '12209' OR PostalCode = '05023';--6--SELECT * FROM Orders
+--WHERE NOT ShipRegion = 'NULL';
+--SELECT * FROM Orders
+
+
+--7
+--SELECT * FROM Customers
+--ORDER BY Country, City 
+
+
+--8 
+
+--INSERT INTO Customers (CustomerID, CompanyName, ContactName, ContactTitle)
+--VALUES ('hand', 'String', 'Jack', 'Legal')
+--SELECT * FROM Customers
+
+--9 
+--UPDATE Orders 
+--SET [ShipRegion] = 'EuroZone'
+--Where ShipCountry = 'France';
+--SELECT  * FROM Orders
+
+
+--10
+--DELETE FROM [Order Details] WHERE Quantity = 1;
+
+--11--
+--SELECT OrderID, CustomerID  
+--FROM Orders
+--WHERE OrderID = '10290'
+
+----12
+--SELECT * FROM Customers
+--Full JOIN Orders ON Customers.CustomerID=Orders.CustomerID 
+
+----13
+--SELECT Employees.FirstName, ReportsTo
+--FROM Employees
+--WHERE ReportsTo is Null
+--14--SELECT Employees.FirstName, ReportsTo
+--FROM Employees--WHERE ReportsTo = 2;--Select * FROM Orders
